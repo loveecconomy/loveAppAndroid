4d7ca843352933cc4ea70de3b1353f2ea80cd59b
eric <kojoaeric@gmail.com>
2018-07-24 14:46:55 -0700

    edited drawer menu

--- app/src/main/java/com/loveeconomy/loveeconomy/Dashboard.java
+++ app/src/main/java/com/loveeconomy/loveeconomy/Dashboard.java
@@ -71,17 +71,17 @@ public class Dashboard extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_message) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_live) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_saved) {
+
+        } else if (id == R.id.nav_logout) {
 
         } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
