2cb978f299e737f0db375e6dd07cf2d33df8afba
eric <kojoaeric@gmail.com>
2018-07-24 07:30:46 -0700

    added launcher

--- app/src/main/java/com/loveeconomy/loveeconomy/MainActivity.java
+++ app/src/main/java/com/loveeconomy/loveeconomy/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.launcher);
 
         SharedPreferences activeSession = this.getSharedPreferences("com.loveeconomy.loveeconomy", Context.MODE_PRIVATE);
         String token = activeSession.getString("auth_token", "");
